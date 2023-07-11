955170af0cf58c18b262a0681a8af317bfff1acb
Roberto Franco <roberto.yuri.franco@gmail.com>
2023-07-10 21:29:42 -0300

    CSS, bootstrap

--- consultorio/app/urls.py
+++ consultorio/app/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('test', views.test, name='test'),
+    path('login', views.login, name='login'),
+    path('create_doctor', views.create_doctor, name='create_doctor'),
 ]
--- consultorio/app/views.py
+++ consultorio/app/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+def test(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def create_doctor(request):
+    return render(request, 'create_doctor.html')
+
+def login(request):
+    return render(request, 'login.html')
